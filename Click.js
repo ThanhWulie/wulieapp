e5f884e791325f40319d722136f352d42d26d9a7
ThanhWulie <vanthanh.10723@gmail.com>
2024-03-23 12:03:03 +0700

    first commit

---
+++ Click.js
@@ -0,0 +1,13 @@
+
+function Funcionaccept() {
+    alert("Accpet Finished.");
+  }
+function Funcionrefuse(){
+    alert("Refuse Finished")
+}
+function Funcionban(){
+    alert("Ban Finished.")
+}
+function Funcionunban(){
+    alert("Unban Finished")
+}
